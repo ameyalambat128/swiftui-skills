979ab991b4c2ffc72bc07688055c1cd6884199a0
Ameya Lambat <63185829+ameyalambat128@users.noreply.github.com>
2026-01-16 23:48:41 -0500

    chore: add cn utility and dark mode support
    - Add clsx and tailwind-merge dependencies
    - Add cn utility function
    - Add dark mode classes to html element

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { IBM_Plex_Sans } from "next/font/google";
 import "./globals.css";
 import Background from "@/components/background";
+import { cn } from "@/lib/utils";
 
 const ibmPlexSans = IBM_Plex_Sans({
   weight: ["300", "400", "500", "600", "700"],
@@ -22,7 +23,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+   <html
+      lang="en"
+      className={cn(
+        "bg-white text-black dark:bg-[#111010] dark:text-white",
+        ibmPlexSans.variable
+      )}
+    >
       <body className={`${ibmPlexSans.variable} font-sans antialiased`}>
         <Background />
         {children}
